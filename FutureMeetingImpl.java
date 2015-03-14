088a23ee4de07c1c81d31ee62e9cda4eb938a851
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-03-14 18:47:18 +0000

    initial version of implementations

---
+++ FutureMeetingImpl.java
@@ -0,0 +1,26 @@
+package ContactManager;
+
+import java.util.Calendar;
+import java.util.Set;
+
+public class FutureMeetingImpl implements FutureMeeting {
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
