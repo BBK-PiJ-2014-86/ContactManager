bb7d60cb9d72ffd22d52c1e178b708b6330f09cb
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-03-14 20:12:59 +0000

    start
    added test to check that method would throw IllegalArgumentException if
    attempting to add an unknown contact

--- TestContactManager.java
+++ TestContactManager.java
@@ -2,13 +2,37 @@ package ContactManager;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestContactManager {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	ContactManager testContactManager;
+	Set <Contact> contacts;
+	Calendar date;
+	
+	/**
+	 * initialising fields in Before to implement before starting 
+	 */
+	@Before
+	public void buildUp () {
+		testContactManager = new ContactManagerImpl();
+		contacts = new HashSet();
+		date = Calendar.getInstance();
+	}
+	
+	@Test (expected= IllegalArgumentException.class)
+	public void addFutureMeetingTest1() {
+		
+		testContactManager.addNewContact("Marcus Eoin", "Interview");
+		contacts.add(new ContactImpl ("Michael Sandison"));
+		date.set(2015, Calendar.JUNE, 9);
+		testContactManager.addFutureMeeting(contacts, date);
 	}
 
 }
