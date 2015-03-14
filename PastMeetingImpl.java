0b151becd3e2c227fc459ceaf30fb6bb62889b05
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-03-14 20:02:48 +0000

    adding fields to class

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -4,6 +4,10 @@ import java.util.Calendar;
 import java.util.Set;
 
 public class PastMeetingImpl implements PastMeeting{
+	
+	private int id;
+	private Calendar date;
+	private Set <Contact> contacts;
 
 	@Override
 	public int getId() {
---
+++ TestContactManager.java
@@ -0,0 +1,14 @@
+package ContactManager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestContactManager {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
