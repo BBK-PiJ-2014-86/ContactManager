5e94adbbc21dac362b86751244c2f539ac8507e6
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-03-29 20:55:08 +0100

    Getters in Storage class
    also made the fields private

--- Storage.java
+++ Storage.java
@@ -5,18 +5,35 @@ import java.io.Serializable;
 import java.util.List;
 
 public class Storage implements Serializable {
-
-	List<Contact> contactList;
-	List<Meeting> meetingList;
-	int contactCount;
-	int idCount;
+	
+	private List<Contact> contactList;
+	private List<Meeting> meetingList;
+	private int contactCount;
+	private int idCount;
 	
 	public Storage (List<Contact> contactList, List<Meeting> meetingList, int contactCount, int idCount) {
-	
+		
 		this.contactList =contactList;
 		this.meetingList = meetingList;
 		this.contactCount = contactCount;
 		this.idCount = idCount;
+	}
+
+	public List<Contact> getContactList() {
+		return contactList;
+	}
+
+	public List<Meeting> getMeetingList() {
+		return meetingList;
+	}
+
+	public int getContactCount() {
+		return contactCount;
+	}
+
+
+	public int getIdCount() {
+		return idCount;
 	}
 	
 	
